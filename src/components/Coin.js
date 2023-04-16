04084c6b7f1b4f6ff1d30eb987b7518210d50d00
Saman <samanezzatabadi@yahoo.com>
2023-04-16 20:00:22 +0430

    some file changed

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,20 +3,27 @@ import React from 'react';
 import styles from '../styles/Coin.module.css';
 
 const Coin = (props) => {
-    let { image, name, symbol, price, marketCap, priceChange } = props;
+  let { image, name, symbol, price, marketCap, priceChange } = props;
 
-    return (
-        <div className={styles.container}>
-            <div>
-                <img src={image} alt="img" />
-                <span className={styles.name}>{name}</span>
-            </div>
-            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
-            <span className={styles.price}>$ {price.toLocaleString()}</span>
-            <span className={ (priceChange > 0) ? styles.redPriceChange : styles.greenPriceChange}>{priceChange.toFixed(2)}</span>
-            <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src={image} alt="img" />
+        <span className={styles.name}>{name}</span>
+      </div>
+      <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+      <span className={styles.price}>$ {price.toLocaleString()}</span>
+      <span
+        className={
+          priceChange > 0 ? styles.redPriceChange : styles.greenPriceChange
+        }
+      >
+        { priceChange && priceChange.toFixed(2)}
+      </span>
+      <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
+      {console.log(priceChange)}
+    </div>
+  );
 };
 
 export default Coin;
